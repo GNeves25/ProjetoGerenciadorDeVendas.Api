b277510af9054d2f4a470e56be33ae37c31b8aad
Guilherme Neves <guilherme.nevescmp@gmail.com>
2021-07-19 23:55:32 -0300

    Implementação dos testes de unidade
    Implementação dos testes de unidade.

---
+++ ProjetoGerenciadorDeVendas.Testes/TestaProduto.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using ProjetoGerenciadorDeVendas.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoGerenciadorDeVendas.Testes
+{
+    public class TestaProduto
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestaConstrutorProduto()
+        {
+            var produto = new Produto("Produto 01", "33.4");
+
+            Assert.AreEqual("Produto 01", produto.Nome);
+            Assert.AreEqual("33.4", produto.Preco);
+        }
+    }
+}
